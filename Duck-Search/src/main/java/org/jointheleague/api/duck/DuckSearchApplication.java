ce11722a88b0d0a8b93c6947ca21fe9e1d4ad63e
Branson778 <bransonator123@gmail.com>
2023-12-24 15:26:47 -0800

    initial

---
+++ Duck-Search/src/main/java/org/jointheleague/api/duck/DuckSearchApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.duck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckSearchApplication.class, args);
+	}
+
+}
---
+++ Duck-Search/src/test/java/org/jointheleague/api/duck/DuckSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.duck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
